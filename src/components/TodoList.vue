cad9eb69ec2377ec331f505d46f38a47fffc8d4c
Yuto-Kishida <kisida0123@icloud.com>
2021-12-07 23:53:42 +0900

    途中筋トレ

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -9,7 +9,6 @@
      </button>
     </div>
     <div>
-<<<<<<< HEAD
       <select v-model="selectedWorkout">
         <option disabled value="">今日はどこの日？</option>
         <option
@@ -22,40 +21,6 @@
       </select>
     </div>
     <div>
-=======
-      筋トレリスト
-      <div class="bui">
-        トレーニングする部位：
-        <select v-model="selectedWorkout">
-          <option disabled value="">今日はどこの部位？</option>
-          <option
-            v-for="workout in optionWorkout"
-            v-bind:value="workout.name"
-            v-bind:key="workout.id"
-          >
-            {{ workout.name }}
-          </option>
-        </select>
-      </div>
-    </div>
-    <div>
-      <div v-if="todos">
-        <div v-for="(todo, index) in todos" v-bind:key="index" class="todo">
-          <div class="todo__checkbox">
-            <input type="checkbox" v-model="todo.isDone" />
-          </div>
-          <div v-if="todo.isDone" class="todo__text todo__text--done">
-            {{ index }}:{{ todo.text }}
-          </div>
-          <div v-else class="todo__text">{{ index }}:{{ todo.text }}</div>
-          <button v-on:click="deleteTodo(index)" class="todo__delete">
-            削除
-          </button>
-        </div>
-      </div>
-    </div>
-    <div class="syumoku">
->>>>>>> 987607cec72735188f60059ac1873062ac47a277
       <div>
         今日やること：
         <input  v-model="inputTodo" placeholder="例：腕立て伏せ 10回 3set"/>
@@ -81,12 +46,6 @@
 <script>
 import firebase from "firebase"
 export default {
-<<<<<<< HEAD
-=======
-  props: {
-    youbi: String,
-  },
->>>>>>> 987607cec72735188f60059ac1873062ac47a277
   data() {
     return {
       muneosusumeON: false,
@@ -99,7 +58,6 @@ export default {
         { id: 5, name: "脚の日" },
       ],
       inputTodo: "",
-<<<<<<< HEAD
       kannsou: "",
       menus: [],
       db: null,
@@ -117,28 +75,13 @@ export default {
       })
       this.todos = obj
     })
-=======
-      todos: [],
-      inputHansei: "",
-      menus: [],
-    }
-  },
-  created: function () {
-    const memo = localStorage.menus
-    this.menus = JSON.parse(memo)
-    for (let i = 0; i < this.menus.length; i++) {
-      if (this.menus[i].youbi === this.youbi) {
-        this.todos.push(this.menus[i].todos)
-      }
-    }
->>>>>>> 987607cec72735188f60059ac1873062ac47a277
+
   },
   methods: {
     makeMO(){
       this.muneosusumeON = true
     },
     addTodo() {
-<<<<<<< HEAD
       if (this.inputTodo === ""){ return }
       this.todosRef.add({
           name: this.inputTodo,
@@ -151,40 +94,6 @@ export default {
       },
     removeTodo(key){
       this.todosRef.doc(key).delete()
-=======
-      if (this.inputTodo !== "") {
-        const todo = { text: this.inputTodo, isDone: false }
-        //this.todos.push(todo)
-        firebase
-          .firestore()
-          .collection("todos")
-          .add(todo)
-          .then((ref) => {
-            this.todos.push({
-              id: ref.todo,
-              ...todo,
-            })
-          })
-      }
-    },
-    deleteTodo(index) {
-      this.todos.splice(index, 1)
-    },
-
-    hozon() {
-      //console.log(this.youbi)
-      const menu = {
-        youbi: this.youbi,
-        selectedWorkout: this.selectedWorkout,
-        todos: this.todos,
-        inputHansei: this.inputHansei,
-      }
-      this.menus.push(menu)
-      //localStorage.menus = JSON.stringify(this.menus)
-      localStorage.setItem("menus", JSON.stringify(this.menus))
-      const art = JSON.parse(localStorage.getItem("menus"))
-      this.menus = art
->>>>>>> 987607cec72735188f60059ac1873062ac47a277
     },
   },
 }
