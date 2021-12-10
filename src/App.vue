a8329ffaea106a1da336ba8e8d79b59306e29afe
Yuto-Kishida <kisida0123@icloud.com>
2021-12-11 00:26:16 +0900

    筋トレ変更２

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
       <router-link to="/fire">firebase</router-link>|
       <router-link to="/mykinntore">筋トレ</router-link>|
       <router-link to="/about">体調管理</router-link>|
-      <router-link to="/calendar">カレンダー</router-link>|
       <button v-if="isLoggin" v-on:click="logOut">ログアウト</button>
       <button v-else v-on:click="logIn">ログイン</button>
     </div>
